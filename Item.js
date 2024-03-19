65c6d0d4b5409ca405993a0b308ac96c1e7515bf
bennydan7 <owusub243@gmail.com>
2024-03-19 16:02:59 +0000

    item backend

---
+++ Item.js
@@ -0,0 +1,68 @@
+const Items = [
+  {
+    id: "1",
+    name: "FOY GRAFFITI TEE",
+    shortDescription: "[BLACK]",
+    longDescription: " ",
+    price: 30,
+    // image: tee1,
+    // image2: tee2,
+    category: "Tees",
+  },
+  {
+    id: "2",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BLACK]",
+    longDescription:
+      "Introducing our stylish collection of hats, available in classic black. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    // image: cap2,
+    // image2: cap2Front,
+    category: "Caps",
+  },
+  {
+    id: "3",
+    name: "FOY ALCATRAZ BEANIE",
+    shortDescription: "[BLACK & WHITE]",
+    longDescription: "Long description for Product 2",
+    price: 15,
+    // image: beanie1,
+    // image2: beanie1,
+    category: "Beanies",
+  },
+  {
+    id: "4",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BABY BLUE]",
+    longDescription:
+      "Introducing our stylish collection of hats, available in classic black and white, as well as sophisticated black paired with a charming baby blue accent. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    // image: cap1,
+    // image2: cap1Front,
+    category: "Caps",
+  },
+  {
+    id: "5",
+    name: "FOY ALCATRAZ BEANIE",
+    shortDescription: "[BROWN & BEIGE]",
+    longDescription:
+      "High-quality image showcasing our alcatraz beanies in various elegant color combinations: brown and beige",
+    price: 15,
+    // image: beanie2,
+    // image2: beanie2,
+    category: "Beanies",
+  },
+  {
+    id: "6",
+    name: "FOY TRUCKER HAT",
+    shortDescription: "[BLACK & WHITE]",
+    longDescription:
+      "Introducing our stylish collection of hats, available in classic black and white. Crafted with premium materials, these hats exude timeless elegance and versatility, perfect for any occasion. Elevate your look with our sleek and modern designs, designed to complement any outfit effortlessly.",
+    price: 20,
+    // image: cap3,
+    // image2: cap3Front,
+    category: "Caps",
+  },
+];
+
+module.exports = Items;
